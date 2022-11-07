5e6c2bf521acdbbce841105a743664fa9a36ffbe
barnabasli <111801819+barnabasli@users.noreply.github.com>
2022-11-07 00:51:58 -0500

    Update retractable.ino

--- retractable.ino
+++ retractable.ino
@@ -1,171 +1,222 @@
-//Does NOT include photoresistors or a tilt sensor but rather utilizes a gyroscope
-//LED RING vars
-#include <FastLED.h>
-#define NUM_LEDS 12
-#define DATA_PIN 3
-#define BRIGHTNESS 100
-CRGB leds[NUM_LEDS]; //array of leds, don't touch
-int ledbrightnesstemp;
-boolean gyros = false;
-boolean lol;
-int blinkredcount;
+// This code is for the non-retractable UltraVii 9.0
 
-//Gyroscope
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 32
+
+//gyroscope
 #include<Wire.h>
-const int MPU_addr=0x68;
+const int MPU_addr = 0x68;
 int16_t AcX,AcY,AcZ,Tmp,GyX,GyY,GyZ;
-int minVal=265;
-int maxVal=402;
+int minVal = 265;
+int maxVal = 402;
 double x;
 double y;
 double z;
-int xAng=0;
-int yAng=0;
-int zAng=0;
+int xAng = 0;
+int yAng = 0;
+int zAng = 0;
+boolean liftedup = false;
 
-//Pad
+//pad
 int padled = 9;
 int padbrightness = 0;
-int padfade = 5; //not used
 
-void setup() { 
-    // init leds
-    FastLED.addLeds<WS2812B, DATA_PIN, RGB>(leds, NUM_LEDS);
-    FastLED.setBrightness(BRIGHTNESS);
-    // init gyro
-    Wire.begin();
-    Wire.beginTransmission(MPU_addr);
-    Wire.write(0x6B);
-    Wire.write(0);
-    Wire.endTransmission(true);
-    Serial.begin(115200);
-    //init pad
-    pinMode(padled, OUTPUT);
-    pinMode(LED_BUILTIN, OUTPUT);
-    padbrightness = 0;
-    gyros = false;
+//display
+#define OLED_RESET     4
+#define SCREEN_ADDRESS 0x3C
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+int timeremaining = 15;
+
+void setup() {
+  if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); 
+  }
+  display.clearDisplay();
+  //initialize pad
+  pinMode(padled, OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
+  padbrightness = 0;
+  //initialize gyroscope
+  Wire.begin();
+  Wire.beginTransmission(MPU_addr);
+  Wire.write(0x6B);
+  Wire.write(0);
+  Wire.endTransmission(true);
+  Serial.begin(115200);
 }
 
-void loop() { 
+void loop() {
   analogWrite(padled, padbrightness);
-  analogWrite(LED_BUILTIN, padbrightness);
-  poweron();
-  int what = 255; // temp variable to hold padbrightness
-  // cleaningcycle
-  for (int k=11; k>=0; k--)
+  //increases pad brightness to max
+  for (int i = 0; i < 255; i++)
   {
-    for (int a=255; a>=0; a--)
-    {
-      leds[k] = CHSV(160, 255, a);
-      FastLED.show();
-      delay(4);
-      gyro();
-    if (x>=30 && x<=180 || x>180 && x<=330) {
-     for (int c=255; c>0; c--)
-     {
-        while (c>100)
-        {
-          padbrightness--;
-          analogWrite(padled, padbrightness);
-          c--;
-        }
-        padbrightness--;
-        analogWrite(padled, padbrightness);
-        Serial.println(padbrightness);
-        delay(3.5);
-     }
-     ledblack();
-     blinkred();
-    while(x>=30 && x<=180 || x>180 && x<=330) //checks x rot
-    {
-      //reruns gyro sequence endlessly while condition is true
-      gyros = true;
-      blinkred();
-    }
-    //after xrot <30, increase pad brightness back to original
-    ledblack();
-    gyros = false;
-    for (int c=0; c<255; c+=5)
-     {
-      for (int i=0; i<=k; i++)
-      {
-        leds[i] = CHSV(160,255,c);
-        FastLED.show();
-      }
-      padbrightness+=5;
-      analogWrite(padled, padbrightness);
-      delay(2);
-      Serial.println(padbrightness);      
-     }
-    a=255;
-    }
-    }
+    padbrightness++;
+    analogWrite(padled, padbrightness);
+    delay(3);
   }
-  // turn off pad leds
-  for (int i=255; i>0; i--)
+  timer();
+  //decreases pad brightness to zero
+  for (int i = 0; i < 255; i++)
   {
     padbrightness--;
     analogWrite(padled, padbrightness);
-    delay(4);
-    Serial.println(padbrightness);
+    delay(3);
   }
   exit(0);
 }
-void blinkred() {
-  ledblack();
-  for (int i=0; i<255; i+=5)
-  {
-    gyro();
-    for (int j=0; j<12; j++)
-    {
-      leds[j] = CHSV(352,255,i);
+
+void batteryindicator() {
+  //draws battery indicator
+  display.setTextColor(SSD1306_WHITE);
+  for (int j = 108; j <= 123; j++) {
+      display.drawPixel(j, 0, SSD1306_WHITE);
+      display.drawPixel(j, 8, SSD1306_WHITE);
+  }
+  for (int j = 0; j <= 8; j++) {
+      display.drawPixel(108, j, SSD1306_WHITE);
+      display.drawPixel(123, j, SSD1306_WHITE);
+  }
+  for (int k = 124; k <= 125; k++) {
+    for (int j = 2; j <= 6; j++) {
+      display.drawPixel(k, j, SSD1306_WHITE);
     }
-    FastLED.show();
-    delay(6);
+  }
+  for (int j = 2; j <= 6; j++) {
+    for (int k = 110; k <= 119; k++) {
+      display.drawPixel(k, j, SSD1306_WHITE);
+    }
   }
 }
-void ledblack(){
-  for (int j=0; j<12; j++)
-  {
-    leds[j] = CHSV(0,0,0);
-    FastLED.show();
+
+void timer() {
+  for (int i = 15; i > 9; i--) {
+    display.clearDisplay();
+    batteryindicator();
+    display.setTextSize(3);
+    display.setCursor(30, 6);
+    display.print(("0:"));
+    display.setCursor(60, 6);
+    display.print((i));
+    display.display();
+    timeremaining = i;
+    for (int j = 0; j < 20; j++) {
+      check();
+      if (liftedup) {
+        display.clearDisplay();
+        batteryindicator();
+        display.setCursor(30, 6);
+        display.print(("0:"));
+        display.setCursor(60, 6);
+        display.print((i));
+        display.display();
+        liftedup = false;
+      }
+      delay(50);
+    }
+    display.clearDisplay();
   }
+  for (int i = 9; i > 0; i--) {
+    batteryindicator();
+    display.setTextSize(3);
+    display.setCursor(25, 6);
+    display.print(("0:0"));
+    display.setCursor(83, 6);
+    display.print((i));
+    display.display();
+    timeremaining = i;
+    for (int j = 0; j < 20; j++) {
+      check();
+      delay(50);
+    }
+    display.clearDisplay();
+  }
+  display.setTextSize(2);
+  display.setCursor(16, 0);
+  display.println(("Cleaning"));
+  display.display();
+  display.setCursor(14, 18);
+  display.println(("Finished!"));
+  display.display();
+  display.clearDisplay();
 }
-void poweron() {
-  for (int j=0; j<11; j++)
-  {
-    leds[j] = CRGB::Black;
-    FastLED.show();
+
+void gyro() {
+  Wire.beginTransmission(MPU_addr);
+  Wire.write(0x3B);
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU_addr,14,true);
+  AcX = Wire.read()<<8|Wire.read();
+  AcY = Wire.read()<<8|Wire.read();
+  AcZ = Wire.read()<<8|Wire.read();
+  xAng = map(AcX,minVal,maxVal,-90,90);
+  yAng = map(AcY,minVal,maxVal,-90,90);
+  zAng = map(AcZ,minVal,maxVal,-90,90);
+  x = RAD_TO_DEG * (atan2(-yAng, -zAng) + PI); //updates values of instance variables
+  y = RAD_TO_DEG * (atan2(-xAng, -zAng) + PI);
+  z = RAD_TO_DEG * (atan2(-yAng, -xAng) + PI);
+}
+
+void check() {
+  gyro();
+  if (x >= 30 && x <= 330) {
+    for (int i = 255; i > 0; i--) {
+      padbrightness--;
+      analogWrite(padled, padbrightness);
+      delay(1.7);
+   }
+  while (x >= 30 && x <= 330) {
+    for (int i = 0; i < 10; i++) {
+      delay(50);
+      gyro();
+      if (x < 30 || x > 330) {
+        goto label1;
+      }
+    }
+    display.clearDisplay();
+    batteryindicator();
+    display.display();
+    for (int i = 0; i < 10; i++) {
+      delay(50);
+      gyro();
+      if (x < 30 || x > 330) {
+        goto label1;
+      }
+    }
+    holdtime();
   }
-  for (int i=0; i<11; i++)
-  {
-    for (int k=0; k<23; k++)
-    {
+  label1:
+    holdtime();
+    for (int i = 0; i < 255; i++) {
       padbrightness++;
       analogWrite(padled, padbrightness);
-      delay(3);
+      delay(3.5);
     }
-    analogWrite(padled, padbrightness);
-    leds[i] = CRGB::Blue;
-    FastLED.show();
-    delay(20);
+  liftedup = true;
   }
-  padbrightness++;
-  padbrightness++;
-  Serial.println(padbrightness);
 }
-void gyro(){
-      Wire.beginTransmission(MPU_addr);
-      Wire.write(0x3B);
-      Wire.endTransmission(false);
-      Wire.requestFrom(MPU_addr,14,true);
-      AcX=Wire.read()<<8|Wire.read();
-      AcY=Wire.read()<<8|Wire.read();
-      AcZ=Wire.read()<<8|Wire.read();
-      xAng = map(AcX,minVal,maxVal,-90,90);
-      yAng = map(AcY,minVal,maxVal,-90,90);
-      zAng = map(AcZ,minVal,maxVal,-90,90);
-      x= RAD_TO_DEG * (atan2(-yAng, -zAng)+PI); //updates values of instance variables
-      y= RAD_TO_DEG * (atan2(-xAng, -zAng)+PI);
-      z= RAD_TO_DEG * (atan2(-yAng, -xAng)+PI);
+
+void holdtime() {
+  display.clearDisplay();
+  if (timeremaining > 9) {
+    batteryindicator();
+    display.setTextSize(3);
+    display.setCursor(30, 6);
+    display.print(("0:"));
+    display.setCursor(60, 6);
+    display.print((timeremaining));
+    display.display();
+  } else {
+    batteryindicator();
+    display.setTextSize(3);
+    display.setCursor(25, 6);
+    display.print(("0:0"));
+    display.setCursor(83, 6);
+    display.print((timeremaining));
+    display.display();
+  }
 }
